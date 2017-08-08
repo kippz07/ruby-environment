bea652c5501aafe2b716f6e077268abd2b8271c9
Jenny Johnson <kippz07@users.noreply.github.com>
2017-08-08 11:02:51 +0100

    initial commit

---
+++ environment/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update -y
+sudo apt-get install ruby2.4 ruby2.4-dev -y
+sudo apt-get install build-essential zlib1g-dev libgmp-dev libxml2 -y
+
+
+sudo gem install bundler
+sudo gem install rake
+sudo gem install rails
+sudo apt-get install nginx -y
